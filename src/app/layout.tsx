4fd9b12c61fc567993b763c027011601e5825caf
Gustav Brochmann <gustavbrochmann@gmail.com>
2025-04-21 14:19:47 +0200

    Favicon updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -108,6 +108,11 @@ export default function RootLayout({
         {/* Preconnect to domains for faster resource loading */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        {/* Favicon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         
         {/* JSON-LD structured data for local business */}
         <Script
